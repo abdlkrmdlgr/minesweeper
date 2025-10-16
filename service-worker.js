a6c4d3472bc827eabe8440778390059f215b327d
Abdülkerim DÜLGER <105209786+abdulkerimdulger@users.noreply.github.com>
2025-10-17 02:24:48 +0300

    Minesweeper PWA - Klasik mayın tarlası oyunu

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'minesweeper-v1';
+const urlsToCache = [
+  './minesweeper.html',
+  './manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache açıldı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch - offline çalışma
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache'de varsa cache'den döndür
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate - eski cache'leri temizle
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
